dee469dd845e9cd0e9b47675bddf3f0cd473efd3
tholenJohn <tholenjohn@gmail.com>
2025-03-09 00:02:59 -0600

    Update AiEncoder.py
    updated logic

--- AiEncoder.py
+++ AiEncoder.py
@@ -10,36 +10,68 @@ import random
 import tkinter as tk
 import pyperclip
 
+#Randomly selected stuff
 random_Invisible_Unicode = ['​','؜'] #zero width
 random_Invisible_Cap_Alphabet = ['󠁁','󠁂','󠁃','󠁄','󠁆','󠁇','󠁈','󠁉','󠁊','󠁋','󠁌','󠁌','󠁍','󠁍','󠁏','󠁐','󠁑','󠁒','󠁓','󠁔','󠁕','󠁖','󠁗','󠁘','󠁙','󠁚']
 random_Invisible_smol_Alphabet = ['󠁡','󠁢','󠁣','󠁤','󠁥','󠁦','󠁧','󠁨','󠁩','󠁪','󠁫','󠁬','󠁭','󠁮','󠁯','󠁰','󠁱','󠁲','󠁳','󠁴','󠁵','󠁶','󠁷','󠁸','󠁹','󠁺']
-visible_Cap_Character_Dict = [['A','Α'],
-                          ['B','В'],
-                          ['C','С'],
-                          ['D','Đ'],
-                          ['E', 'Ε'],
-                          ['F',],
-                          ['G',],
-                          ['H',],
-                          ['I',''],
-                          [],
-                          [],
-                          [],
-                          [],
-                          [],
-                          [],
-                          [],
-                          [],
-                          [],
-                          [],
-                          [],
-                          [],
-                          [],
-                          [],
-                          [],
-                          [],
-                          []
-                          ]
+
+#visible to user
+visible_Cap_Character_Dict = {'A': 'Α',
+                              'B': 'В',
+                              'C': 'С',
+                              'D': 'Đ',
+                              'E': 'Ε',
+                              'F': 'Ḟ',
+                              'G': 'G', #normal latin character
+                              'H': 'Η',
+                              'I': 'Ι',
+                              'J': 'Ј',
+                              'K': 'Κ',
+                              'L': 'L', #normal latin character
+                              'M': 'Μ',
+                              'N': 'Ν',
+                              'O': 'Ο',
+                              'P': 'Ρ',
+                              'Q': 'Q', #normal latin character
+                              'R': 'Ɍ',
+                              'S': 'Ѕ',
+                              'T': 'Τ',
+                              'U': 'Ų',
+                              'V': 'Ѵ',
+                              'W': 'Ш',
+                              'X': 'Χ',
+                              'Y': 'Υ',
+                              'Z': 'Ζ'}
+visible_smol_Character_Dict = {'a':'ɑ',
+                               'b':'ƅ',
+                               'c':'с',
+                               'd':'ď',
+                               'e':'е',
+                               'f':'f', #normal latin character
+                               'g':'ɡ', 
+                               'h':'һ',
+                               'i':'ί',
+                               'j':'ϳ',
+                               'k':'ҟ',
+                               'l':'ḷ', 
+                               'm':'ՠ',
+                               'n':'п',
+                               'o':'ο',
+                               'p':'р',
+                               'q':'ɋ', 
+                               'r':'r', #normal latin character
+                               's':'ѕ',
+                               't':'t', #normal latin character
+                               'u':'џ',
+                               'v':'ν',
+                               'w':'ɯ',
+                               'x':'х',
+                               'y':'у',
+                               'z':'ẓ',
+                               }
+
+# how many of the random invisible characters added between each visible character. 
+# The higher the number the more characters it takes up.
 min_Insert_Value_Between_Characters = 2
 max_Insert_Value_Between_Characters = 10
 current_List_List = [id(random_Invisible_Cap_Alphabet),
@@ -51,17 +83,24 @@ root = tk.Tk()
 root.geometry('650x480')
 root.title("Encode Your Text")
 
+
 label= tk.Label(root, text="Paste text here: ")
 # label.grid(row=0,column=0)
 
 
 textField = tk.Text(root, state='disabled')
 # textField.grid(row=1, columnspan=3)
+# output is held in memory for copying to clipboard. This text isn't displayed
 output = tk.Text(root, state='normal')
+
 entry = tk.Entry(root, width=50)
 # entry.grid(row=0, column=1)
 
 def grabRandomFromList(id_number):
+    """
+    Returns:
+        random unicode character based on whatever list
+    """
     if(id(random_Invisible_Unicode) == id_number):
         return random_Invisible_Unicode[random.randint(1,100) % len(random_Invisible_Unicode)]
     if(id(random_Invisible_Cap_Alphabet) == id_number):
@@ -71,128 +110,33 @@ def grabRandomFromList(id_number):
     return "ERROR"
 
 def encodeOutput():
+    """
+    Grabs what user typed in textField and encodes it.
+    
+    Returns:
+        sets value of textfield and copies encodedOutput to pastebin with pyperclip
+    """
     entrystring = entry.get()
     textField.config(state='normal')
     textField.delete('1.0',tk.END)
     output.delete('1.0',tk.END)
     # entry.delete(0, tk.END)
-    encodedOutput = ''
+    encodedOutput = '‭'
     for character in entrystring:
         for i in range(random.randint(min_Insert_Value_Between_Characters,max_Insert_Value_Between_Characters)):
-            # if random.randint(1,100) % current_List_Count == 
-            # encodedOutput += random_Invisible_Unicode[random.randint(1,100) % len(random_Invisible_Unicode)]
             encodedOutput += grabRandomFromList(current_List_List[random.randint(1,100) % len(current_List_List)])
-        if character == 'A':
-            encodedOutput +='Α'
-        elif character == 'B':
-            encodedOutput +='В'
-        elif character == 'C':
-            encodedOutput +='С'
-        elif character == 'D':
-            encodedOutput +='Đ'
-        elif character == 'E':
-            encodedOutput +='Ε'
-        elif character == 'F':
-            encodedOutput +='Ḟ'
-        elif character == 'G':
-            encodedOutput +='G' #normal latin character
-        elif character == 'H':
-            encodedOutput +='Η'
-        elif character == 'I':
-            encodedOutput +='Ι'
-        elif character == 'J':
-            encodedOutput +='Ј'
-        elif character == 'K':
-            encodedOutput +='Κ'
-        elif character == 'L':
-            encodedOutput +='L' #normal latin character
-        elif character == 'M':
-            encodedOutput +='Μ'
-        elif character == 'N':
-            encodedOutput +='Ν'
-        elif character == 'O':
-            encodedOutput +='Ο'
-        elif character == 'P':
-            encodedOutput +='Ρ'
-        elif character == 'Q':
-            encodedOutput +='Q' #normal latin character
-        elif character == 'R':
-            encodedOutput +='Ɍ'
-        elif character == 'S':
-            encodedOutput +='Ѕ'
-        elif character == 'T':
-            encodedOutput +='Τ'
-        elif character == 'U':
-            encodedOutput +='Ų'
-        elif character == 'V':
-            encodedOutput +='Ѵ'
-        elif character == 'W':
-            encodedOutput +='Ш'
-        elif character == 'X':
-            encodedOutput +='Χ'
-        elif character == 'Y':
-            encodedOutput +='Υ'
-        elif character == 'Z':
-            encodedOutput +='Ζ'
-        elif character == 'a':
-            encodedOutput +='ɑ'
-        elif character == 'b':
-            encodedOutput +='ƅ'
-        elif character == 'c':
-            encodedOutput +='с'
-        elif character == 'd':
-            encodedOutput +='ď'
-        elif character == 'e':
-            encodedOutput +='е'
-        elif character == 'f':
-            encodedOutput +='f' #normal latin character
-        elif character == 'g':
-            encodedOutput +='ɡ'
-        elif character == 'h':
-            encodedOutput +='һ'
-        elif character == 'i':
-            encodedOutput +='ί'
-        elif character == 'j':
-            encodedOutput +='ϳ'
-        elif character == 'k':
-            encodedOutput +='ҟ'
-        elif character == 'l':
-            encodedOutput +='ḷ'
-        elif character == 'm':
-            encodedOutput +='ՠ'
-        elif character == 'n':
-            encodedOutput +='п'
-        elif character == 'o':
-            encodedOutput +='ο'
-        elif character == 'p':
-            encodedOutput +='р'
-        elif character == 'q':
-            encodedOutput +='ɋ'
-        elif character == 'r':
-            encodedOutput +='r' #normal latin character
-        elif character == 's':
-            encodedOutput +='ѕ'
-        elif character == 't':
-            encodedOutput +='t' #normal latin character
-        elif character == 'u':
-            encodedOutput +='џ'
-        elif character == 'v':
-            encodedOutput +='ν'
-        elif character == 'w':
-            encodedOutput +='ɯ'
-        elif character == 'x':
-            encodedOutput +='х'
-        elif character == 'y':
-            encodedOutput +='у'
-        elif character == 'z':
-            encodedOutput +='ẓ'
+        if character in visible_Cap_Character_Dict.keys():
+            encodedOutput += visible_Cap_Character_Dict[character]
+        elif character in visible_smol_Character_Dict.keys():
+            encodedOutput += visible_smol_Character_Dict[character]
         else:
             encodedOutput += character
-        # encodedOutput += character
-    # entry.insert(0, encodedOutput)
-    textField.insert('1.0',encodedOutput)
+    
+    textField.insert('1.0',"Encoded text below may look like there are no spaces but there are.\n")
+    textField.insert(tk.END,encodedOutput)
     textField.insert(tk.END,"\n\nCOPIED TO CLIPBOARD")
     textField.config(state='disabled')
+
     output.insert('1.0',encodedOutput)
     copy_text()
 
